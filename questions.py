c830c70c0cbf49a0f870c10bf62324428ecb4a16
MatiasEmmanuelPerez <matiasperezinformatica@gmail.com>
2025-03-19 19:47:23 -0300

    combinarlistas

--- questions.py
+++ questions.py
@@ -27,15 +27,16 @@ correct_answers_index = [1, 2, 0, 3, 1]
 # puntos del usuario
 puntos = 0
 
-# El usuario deberá contestar 3 preguntas
-for _ in range(3):
+# combina las 3 listas en una sola y selecciona 3 preguntas aleatorias(que pueden repetirse) cada una con sus posibles soluciones y el indice de la respuesta correcta
+questions_to_ask = random.choices(list(zip(questions, answers, correct_answers_index)), k=3)
 
-    # Se selecciona una pregunta aleatoria
-    question_index = random.randint(0, len(questions) - 1)
+
+# El usuario deberá contestar 3 preguntas
+for question, answer, correct_answer_index in questions_to_ask:
 
     # Se muestra la pregunta y las respuestas posibles
-    print(questions[question_index])
-    for i, answer in enumerate(answers[question_index]):
+    print(question)
+    for i, answer in enumerate(answer):
         print(f"{i + 1}. {answer}")
 
     # El usuario tiene 2 intentos para responder correctamente
@@ -52,7 +53,7 @@ for _ in range(3):
         
 
         # Se verifica si la respuesta es correcta
-        if user_answer == correct_answers_index[question_index]:
+        if user_answer == correct_answer_index:
             print("¡Correcto!")
             puntos += 1  # si es correcta suma 
             break
@@ -60,7 +61,7 @@ for _ in range(3):
         # Si el usuario no responde correctamente después de 2 intentos,
         # se muestra la respuesta correcta
         print("Incorrecto. La respuesta correcta es:")
-        print(answers[question_index][correct_answers_index[question_index]])
+        print(correct_answer_index + 1)
         puntos -= 0.5 # si es incorrecta resta
 
     # Se imprime un blanco al final de la pregunta
